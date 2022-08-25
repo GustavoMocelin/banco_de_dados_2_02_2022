4f89e5a86830f46295e9231b43b71f1c6e89fe74
GUSTAVO MOCELIN PROCOPIO <gustavo.9942@aluno.pr.senac.br>
2022-08-24 22:22:54 -0300

    aula join

--- aula5.sql
+++ aula5.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS aprenderjoin CHARACTER SET utf8mb4;
 
 use aprenderjoin;
 
+DROP TABLE IF EXISTS restaurantes;
 DROP TABLE IF EXISTS cidade;
 DROP TABLE IF EXISTS estado;
 
@@ -36,3 +37,138 @@ SET @idBahia:=(SELECT id_estado FROM estado WHERE estado.nome = "Bahia");
 SET @idPernambuco:=(SELECT id_estado FROM estado WHERE estado.nome = "Pernambuco");
 SET @idPara:=(SELECT id_estado FROM estado WHERE estado.nome = "Para");
 SET @idMatoGrosso:=(SELECT id_estado FROM estado WHERE estado.nome = "Mato Grosso");
+
+INSERT INTO cidade (nome, id_estado) VALUES
+('Curitiba', @idParana),
+('Londrina', @idParana),
+('Paranagua', @idParana),
+('Sao Paulo', @idSaoPaulo),
+('Sorocaba', @idSaoPaulo),
+('Rio de Janeiro', @idRioDeJaneiro),
+('Niteroi', @idRioDeJaneiro),
+('Mage', @idRioDeJaneiro),
+('Porto Real', @idRioDeJaneiro),
+('Petropoles', @idRioDeJaneiro),
+('Salvador', @idBahia),
+('Feira de Santana', @idBahia),
+('Recife', @idPernambuco),
+('Olinda', @idPernambuco),
+('Petrolina', @idPernambuco),
+('Belem', @idPara),
+('Cuiaba', @idMatoGrosso);
+
+SELECT * FROM cidade;
+
+SELECT nome FROM estado Where id_estado =  (SELECT id_estado FROM cidade WHERE nome = "Petropoles");
+
+SELECT * FROM cidade INNER JOIN estado ON cidade.id_estado = estado.id_estado WHERE cidade.nome = 'Petropoles';
+
+
+CREATE TABLE IF NOT EXISTS restaurantes (
+    id_restaurante INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    descricao VARCHAR(255),
+    id_cidade INTEGER,
+    FOREIGN KEY (id_cidade) REFERENCES cidade(id_cidade)
+);
+
+SET @idCuritibaCidade := (SELECT id_cidade FROM  cidade WHERE nome = 'Curitiba');
+SET @idSaoPauloCidade := (SELECT id_cidade FROM cidade WHERE nome = 'Sao Paulo');
+SET @idRioDeJaneiroCidade := (SELECT id_cidade FROM cidade WHERE nome = 'Rio de Janeiro');
+SET @idPetropolesCidade := (SELECT id_cidade FROM cidade WHERE nome = 'Petropoles');
+SET @idFeiraDeSantanaCidade := (SELECT id_cidade FROM cidade WHERE nome = 'Feira de Santana');
+
+select @idFeiraDeSantanaCidade as 'cidade';
+
+INSERT INTO restaurantes (nome, descricao, id_cidade) VALUES
+    ('Comida mineira', 'Comida tipica de minas', @idCuritibaCidade),
+    ('Fogo de Chao', 'Serve costela de fogo de chao', @idSaoPauloCidade),
+    ('Frangao', 'Serve frangao', @idRioDeJaneiroCidade),
+    ('Pizza Boa', 'Serve pizza', @idPetropolesCidade),
+    ('Pertuti', 'Comida italiana', @idFeiraDeSantanaCidade);
+
+SELECT * FROM restaurantes;
+
+
+
+SELECT 'Com restaurantes antes --------------' as 'INFO';
+SELECT * FROM restaurantes INNER JOIN cidade ON restaurantes.id_cidade = cidade.id_cidade WHERE restaurantes.nome = 'Comida mineira';
+
+
+SELECT 'Com cidade antes --------------' as 'INFO';
+SELECT * FROM cidade INNER JOIN restaurantes ON restaurantes.id_cidade = cidade.id_cidade WHERE restaurantes.nome = 'Frangao';
+
+
+SELECT 'Controlando colunas --------------' as 'INFO';
+SELECT 
+    cidade.nome as nome_cidade,
+    restaurantes.nome as nome_do_restaurante,
+    restaurantes.descricao as descricao_do__restaurante
+FROM 
+    cidade 
+INNER JOIN 
+    restaurantes 
+        ON restaurantes.id_cidade = cidade.id_cidade 
+WHERE 
+    restaurantes.nome = 'Comida Mineira';
+
+
+SELECT 'Com like --------------' as 'INFO';
+
+SELECT 
+    cidade.nome as nome_cidade,
+    restaurantes.nome as nome_do_restaurante,
+    restaurantes.descricao as descricao_do__restaurante
+FROM 
+    cidade 
+INNER JOIN 
+    restaurantes 
+        ON restaurantes.id_cidade = cidade.id_cidade 
+WHERE 
+    restaurantes.nome LIKE '%M%';
+
+
+SELECT 'transformacao em uma tabela --------------' as 'INFO';
+
+SELECT 
+    t1.nome_cidade,
+    t1.nome_do_restaurante
+FROM
+(
+    SELECT 
+    cidade.nome as nome_cidade,
+    restaurantes.nome as nome_do_restaurante,
+    restaurantes.descricao as descricao_do__restaurante
+    FROM
+        cidade 
+    INNER JOIN 
+        restaurantes 
+            ON restaurantes.id_cidade = cidade.id_cidade 
+    WHERE 
+        restaurantes.nome LIKE '%M%' 
+) as t1
+ 
+WHERE
+    t1.nome_cidade = 'Curitiba'
+;
+
+
+SELECT 'com mais de um INNER JOIN --------------' as 'INFO';
+
+SELECT 
+    cidade.nome as nome_cidade,
+    estado.nome as nome_do_estado,
+    restaurantes.nome as nome_do_restaurante
+FROM
+    restaurantes
+INNER JOIN
+    cidade
+        ON restaurantes.id_cidade = cidade.id_cidade
+INNER JOIN
+    estado
+        ON cidade.id_estado = estado.id_estado
+WHERE 
+    restaurantes.nome LIKE 'Comida M%';
+
+
+
